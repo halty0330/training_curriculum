99d93439ce0a2c560b0ea8e3b26190057f1619f7
halty0330 <hal0330t0321@icloud.com>
2021-08-28 09:07:25 +0900

    Issue4~6完成

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:日付を選択, :date, :予定を入力, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
